edb6e9497df2bb188a3e05859108a6916f0a1986
fred-penot <frederic.penot@gmail.com>
2016-12-06 23:25:16 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -1,7 +1,7 @@
 login_ssh="docker"
 
 service ssh start > /home/${login_ssh}/services.log
-service apache2 start > /home/${login_ssh}/services.log
+service apache2 start >> /home/${login_ssh}/services.log
 service tvheadend start >> /home/${login_ssh}/services.log
 chmod -f 777 /home/${login_ssh}/services.log
 chmod -Rf 777 /home/hts/.hts/tvheadend
