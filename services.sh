1b3de5d08f59454539d1b8a8e411911b674e1ae3
fred-penot <frederic.penot@gmail.com>
2016-12-06 17:32:28 +0100

    Update services.sh

--- services.sh
+++ services.sh
@@ -4,3 +4,4 @@ service ssh start > /home/${login_ssh}/services.log
 service apache2 start > /home/${login_ssh}/services.log
 service tvheadend start >> /home/${login_ssh}/services.log
 chmod -f 777 /home/${login_ssh}/services.log
+chmod -Rf 777 /home/hts/.hts/tvheadend
