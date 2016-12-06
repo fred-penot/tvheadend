d09bc9a202daa260ab28541ab13cadbe53075f3e
fred-penot <frederic.penot@gmail.com>
2016-12-06 20:47:19 +0100

    Update import_logo.sh

--- import_logo.sh
+++ import_logo.sh
@@ -1,6 +1,7 @@
 login_ssh=docker
 
-cd /var/www/html
+mkdir -p /home/apps
+cd /home/apps
 git clone https://github.com/fred-penot/logotv.git
-chmod -Rf 777 /var/www/html/logotv
-chown -R ${login_ssh}:${login_ssh} /var/www/html/logotv
+chmod -Rf 777 /home/apps/logotv
+chown -R ${login_ssh}:${login_ssh} /home/apps
